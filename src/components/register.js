a74a4fc66801369a5758114d96fc7efb22835e36
Ivan <vankata294n4@gmail.com>
2023-02-23 16:25:49 +0200

    Understanding useRef

--- src/components/register.js
+++ src/components/register.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 export default function Register(props) {
    
     const [formData, setData] = useState({username: '', password: ''})
-
+    const infoRef = useRef()
 
     const onChangeHandler = (e) => {
         setData(prev => (
@@ -14,12 +14,20 @@ export default function Register(props) {
         console.log(formData.username, formData.password)
     }
 
+    useEffect(() => {
+        if (formData.username && formData.password) {
+            infoRef.current.value = `user: ${formData.username} -> pass: ${formData.password}`
+        }
+    }, [formData.username, formData.password])
+
     return (<div>
                 <label>Username</label>
                 <input name="username" value={formData.username} onChange={(e) => onChangeHandler(e)}></input> 
                 <label>Password</label>
                 <input name="password" value={formData.password} onChange={(e) => onChangeHandler(e)}></input> 
                 <button onClick={clickHandler}>info</button>
+
+                <input id="uncontrolledInput" name="uncontrolled" ref={infoRef}></input>
             </div>)
 
 };
